ed33b134a5164071948c8a0da55bbcb695a727a1
FirmTech <atldrake532@gmail.com>
2020-02-05 21:53:41 +0000

    taxService - Interface and Implementation

---
+++ Firmpay.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmpay.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
---
+++ Firmpay.Services/Implementation/TaxService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmpay.Services.Implementation
+{
+    public class TaxService : ITaxService
+    {
+        private decimal taxRate;
+        private decimal tax;
+        public decimal TaxAmount(decimal totalAmount)
+        {
+            if (totalAmount <= 1000)
+            {
+                //Tax free Rate
+                taxRate = .0m;
+                tax = totalAmount * taxRate;
+            }
+            else if (totalAmount > 1000 && totalAmount <= 3000)
+            {
+
+            }
+        }
+    }
+}
