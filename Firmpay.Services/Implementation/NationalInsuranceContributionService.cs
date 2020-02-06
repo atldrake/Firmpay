36df25e7ba8946fe984903d0dc9e579aeb6a772d
FirmTech <atldrake532@gmail.com>
2020-02-06 07:56:17 +0000

    NationalInsuranceContributionService

---
+++ Firmpay.Services/Implementation/NationalInsuranceContributionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmpay.Services.Implementation
+{
+    public class NationalInsuranceContributionService : INationalInsuranceContributionService
+    {
+        private decimal NIRate;
+        private decimal NIC; 
+        public decimal NIContribution(decimal totalAmount)
+        {
+            //
+        }
+    }
+}
--- Firmpay.Services/Implementation/TaxService.cs
+++ Firmpay.Services/Implementation/TaxService.cs
@@ -18,8 +18,26 @@ namespace Firmpay.Services.Implementation
             }
             else if (totalAmount > 1000 && totalAmount <= 3000)
             {
-
+                //Basic Tax Rate
+                taxRate = .20m;
+                //Income Tax
+                tax = (1000 * .0m) + ((totalAmount - 1000) * taxRate);
             }
+            else if (totalAmount > 3000 && totalAmount <= 12500)
+            {
+                //Higher  Tax Rate
+                taxRate = .40m;
+                //Income Tax 
+                tax = (1000 * .0m) + ((3000 - 1000) * .20m) + ((totalAmount - 3000) * taxRate);
+            }
+            else if (totalAmount > 12500)
+            {
+                //Additional tax rate
+                taxRate = .45m;
+                tax = (1000 * .0m) + ((3000 - 1000) * .20m) + 
+                    ((12500 - 3000) * .40m) + ((totalAmount - 12500) * taxRate);
+            }
+            return tax;
         }
     }
 }
